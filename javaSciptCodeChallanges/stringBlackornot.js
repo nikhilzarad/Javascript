7f26e8140c382119dea3daa13229980c69f8fac7
Nikhil <nikhil.zarad@infosys.com>
2024-08-22 16:41:53 +0530

    String blank or not

---
+++ javaSciptCodeChallanges/stringBlackornot.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function to check whether a string is blank or not.
+
+function strBlankorNot(input) {
+  if (input.length == "") {
+    return true;
+  } else 
+  return false;
+}
+console.log(strBlankorNot(""));
+console.log(strBlankorNot("abc"));
